56e2dcbb8d46023e73de15334345718df01f7027
MTorrejon <torrejon.1707@gmail.com>
2024-11-11 23:36:35 -0300

    Error en la validación de restaurantes (falta reorganizar SelectRoleActivity.kt)

--- app/src/main/java/com/pedrodev/tabletrack/SelectRoleActivity.kt
+++ app/src/main/java/com/pedrodev/tabletrack/SelectRoleActivity.kt
@@ -20,7 +20,8 @@ import kotlin.random.Random
 class SelectRoleActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     private lateinit var binding: ActivitySelectRoleBinding
     private lateinit var auth: FirebaseAuth
-    private var userRole: Int? = null
+    private var roleInt: Int? = null
+    private var roleString: String? = null
     private var randomCodeLength = 6
     val charactersInCode : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
 
@@ -57,7 +58,8 @@ class SelectRoleActivity : AppCompatActivity(), AdapterView.OnItemSelectedListen
 
         binding.buttonSignUp.setOnClickListener {
             val nameOrCode = binding.editTextNameOrCode.text.toString().trim()
-            val randomCode = generateRandomCodeRestaurant()
+            var randomCode = generateRandomCodeRestaurant()
+            var validRestaurant = false
 
             binding.back.isClickable = false
             binding.linearLayout.visibility = View.GONE
@@ -74,11 +76,14 @@ class SelectRoleActivity : AppCompatActivity(), AdapterView.OnItemSelectedListen
                         val userData = hashMapOf(
                             "username" to username,
                             "email" to email,
+                            "role" to roleString,
+                            "restaurantID" to randomCode,
                             "timeCreation" to Timestamp.now()
                         )
-                        // AQUÍ FALTA QUE SE CREE EL RESTAURANTE O QUE SE UNA A UN RESTAURANTE EXISTENTE
-                        when (userRole) {
+                        // AQUÍ FALTA QUE SE UNA A UN RESTAURANTE EXISTENTE (when 2, 3)
+                        when (roleInt) {
                             1 -> {
+                                Log.d("ERROR", "ROL ES ADMINISTRADOOOOOOOOOR")
                                 val restaurantData = hashMapOf(
                                     "name" to nameOrCode,
                                     "adminID" to userID,
@@ -87,34 +92,78 @@ class SelectRoleActivity : AppCompatActivity(), AdapterView.OnItemSelectedListen
 
                                 db.collection("restaurants").document(randomCode).set(restaurantData)
                                     .addOnSuccessListener {
-                                        Log.d("TAG", "FUNCIONÓ, name: $nameOrCode | code: $randomCode ")
+                                        Log.d("TAG",
+                                            "FUNCIONÓ, name: $nameOrCode | code: $randomCode ")
                                     }
                                     .addOnFailureListener {
-                                        Log.d("ERROR", "aaaaaaaaaaaaaa")
+                                        Log.d("ERROR",
+                                            "falló agregar restaurant a db")
                                     }
-
                             }
-                            2, 3 -> {
+                            else -> {
+                                Log.d("ERROR", "CUALQUIER OTRO ROOOOOOOOL")
+                                randomCode = nameOrCode
 
+                                db.collection("restaurants").document(randomCode).get()
+                                    .addOnSuccessListener {
+                                        if (it.exists()) {
+                                            validRestaurant = true
+                                            Log.d("ERROR", "RESTAURANTE SI EXISTEEEEE")
+                                            db.collection("users").document(userID.toString())
+                                                .update("restaurantID", randomCode)
+                                                .addOnSuccessListener {
+                                                    Log.d("TAG",
+                                                        "usuario asociado con restaurante" +
+                                                                "user: $username | restaurantecode: $nameOrCode")
+                                                }
+                                                .addOnFailureListener {
+                                                    Log.d("ERROR",
+                                                        "falló asociar usuario y restaurant")
+                                                }
+                                        } else {
+                                            Log.d("ERROR", "NO EXISTE RESTAURANTEEEEEE")
+                                            validRestaurant = false
+                                            binding.linearLayout.visibility = View.VISIBLE
+                                            binding.linearLayoutNameCode.visibility = View.VISIBLE
+                                            binding.progressBar.visibility = View.GONE
+                                            binding.back.isClickable = true
+
+                                            binding.root.alert("No existe restaurante con ese código. Intente de nuevo")
+                                            return@addOnSuccessListener
+                                        }
+                                    }
                             }
-                            else -> TODO()
+                        }
+                        Log.d("ERROR", "validRestaurant = $validRestaurant")
+                        Log.d("ERROR", "validRestaurant = $validRestaurant")
+                        Log.d("ERROR", "validRestaurant = $validRestaurant")
+                        Log.d("ERROR", "validRestaurant = $validRestaurant")
+
+                        if (!validRestaurant) {
+                            Log.d("ERROR", "VALID RESTAURANT FAAAAAAAAAAALSE")
+                            return@addOnCompleteListener
                         }
 
                         userID?.let {
                             db.collection("users").document(it).set(userData)
                                 .addOnSuccessListener {
+                                    Log.d("ERROR", "USERID LET SUCCESS")
                                     this.moveTo(TableMapActivity::class.java)
                                     Functions.clearData(this, "temp_data")
                                     finish()
                                 }
                                 .addOnFailureListener {
+                                    Log.d("ERROR", "USERID LET FAILURE")
                                     binding.root.alert(getString(R.string.failed_database))
                                     binding.buttonSignUp.isClickable = true
                                 }
                         }
+                        Log.d("ERROR", "ultimo validRestaurant = $validRestaurant")
+
                     } else {
                         binding.root.alert(getString(R.string.failed_sign_up))
                         binding.buttonSignUp.isClickable = true
+
                     }
                 }
         }
@@ -131,23 +180,36 @@ class SelectRoleActivity : AppCompatActivity(), AdapterView.OnItemSelectedListen
                 binding.textNameOrCode.text = getString(R.string.text_restaurant_name)
                 binding.editTextNameOrCode.hint = getString(R.string.hint_restaurant_name)
                 binding.linearLayoutNameCode.visibility = View.VISIBLE
+                roleString = "admin"
+                roleInt = 1
             }
-            2, 3 -> { // MESEROS Y RECEPCIÓN
+            2 -> { // MESEROS Y RECEPCIÓN
                 binding.textNameOrCode.text = getString(R.string.text_restaurant_code)
                 binding.editTextNameOrCode.hint = getString(R.string.hint_restaurant_code)
                 binding.linearLayoutNameCode.visibility = View.VISIBLE
+                roleString = "waiter"
+                roleInt = 2
+            }
+            3 -> {
+                binding.textNameOrCode.text = getString(R.string.text_restaurant_code)
+                binding.editTextNameOrCode.hint = getString(R.string.hint_restaurant_code)
+                binding.linearLayoutNameCode.visibility = View.VISIBLE
+                roleString = "receptionist"
+                roleInt = 3
             }
             else -> binding.linearLayoutNameCode.visibility = View.GONE
         }
-        userRole = pos
+
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
-        // aquí hasta ahora no iría nada, pero cuando se borra tira un error xd
+        // aquí hasta ahora no iría nada, pero cuando se borra la función tira un error xd
     }
 
     private fun generateRandomCodeRestaurant() = (1..randomCodeLength)
         .map { Random.nextInt(0, charactersInCode.size).let { charactersInCode[it]} }
         .joinToString("")
+        // esto genera el código para el restaurante de 6 caracteres
+
 
 }

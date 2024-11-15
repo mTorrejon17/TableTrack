b3a55fc9fc0b50f6a075d1c657c2f96a0a263eb5
MTorrejon <torrejon.1707@gmail.com>
2024-11-15 00:49:53 -0300

    SelectRoleActivity.kt #3. Se cae si se intenta registrar un correo ya registrado

--- app/src/main/java/com/pedrodev/tabletrack/SelectRoleActivity.kt
+++ app/src/main/java/com/pedrodev/tabletrack/SelectRoleActivity.kt
@@ -7,7 +7,6 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
 import com.google.android.gms.tasks.Tasks
 import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
@@ -25,6 +24,8 @@ class SelectRoleActivity : AppCompatActivity(), AdapterView.OnItemSelectedListen
     private var roleInt: Int? = null
     private var roleString: String? = null
     private var randomCodeLength = 6
+    var restaurantName = ""
+    var restaurantCode = ""
     val charactersInCode : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,6 +41,7 @@ class SelectRoleActivity : AppCompatActivity(), AdapterView.OnItemSelectedListen
         val username = Functions.getString(this, "temp_data", "username")
         val email = Functions.getString(this, "temp_data", "email")
         val password = Functions.getString(this, "temp_data", "password")
+
 
         // ESTO PONE LAS OPCIONES DEL strings.xml EN EL SPINNER
         val spinner: Spinner = binding.spinnerRole
@@ -65,233 +67,116 @@ class SelectRoleActivity : AppCompatActivity(), AdapterView.OnItemSelectedListen
             binding.progressBar.visibility = View.VISIBLE
 
             val db = FirebaseFirestore.getInstance()
-            val user = auth.currentUser
-            val userID = user?.uid
-            var validCode = false
+            var user = auth.currentUser
+            var userID = user?.uid
+            var stopFunction = false
 
             val task = when (roleInt) {
                 1 -> {
-                    null
+                    restaurantName = binding.editTextNameOrCode.text.toString().trim()
+                    restaurantCode = generateRandomCodeRestaurant()
+                    val userData = hashMapOf(
+                        "username" to username,
+                        "email" to email,
+                        "role" to roleString,
+                        "memberOf" to restaurantCode,
+                        "timeCreation" to Timestamp.now()
+                    )
+                    val restaurantData = hashMapOf(
+                        "name" to restaurantName,
+                        "adminID" to userID,
+                        "timeCreation" to Timestamp.now()
+                    )
+
+                    db.collection("restaurants").document(restaurantCode).set(restaurantData)
+                        .addOnSuccessListener {
+                            Log.d("TAG",
+                                "RESTAURANTE AGREGADO A DB, name: $restaurantName | code: $restaurantCode ")
+                        }
+                        .addOnFailureListener {
+                            Log.d("ERROR",
+                                "FALLÓ agregar restaurant a db")
+                        }
+
+                    db.collection("users").document(userID.toString()).set(userData)
+                        .addOnSuccessListener {
+                            Log.d("TAG",
+                                "ADMIN AGREGADO A DB, name: $username | member of: $restaurantCode ")
+                        }
+                        .addOnFailureListener {
+                            Log.d("ERROR",
+                                "FALLÓ agregar admin a db")
+                        }
                 }
                 2, 3 -> {
-                    val restaurantCode = binding.editTextNameOrCode.text.toString().trim()
+                    restaurantCode = binding.editTextNameOrCode.text.toString().trim()
                     db.collection("restaurants").document(restaurantCode).get()
                         .addOnSuccessListener { document ->
                             if (!document.exists()) {
-                                Log.e("TAG", "ERROR. El código $restaurantCode no está registrado.")
+                                Log.e("ERROR", "ERROOOR El código $restaurantCode no está registrado")
                                 binding.root.alert("Código ingresado no existe.")
                                 binding.linearLayout.visibility = View.VISIBLE
                                 binding.linearLayoutNameCode.visibility = View.VISIBLE
                                 binding.progressBar.visibility = View.GONE
-                                validCode = false
+                                stopFunction = true
                             } else {
-                                validCode = true
+                                val userData = hashMapOf(
+                                    "username" to username,
+                                    "email" to email,
+                                    "role" to roleString,
+                                    "memberOf" to restaurantCode,
+                                    "timeCreation" to Timestamp.now()
+                                )
+
+                                db.collection("users").document(userID.toString()).set(userData)
+                                    .addOnSuccessListener {
+                                        Log.d("TAG", "USER $username rol $roleString añadido correctamente a la db")
+                                    }
+                                    .addOnFailureListener {
+                                        Log.e("ERROR", "FALLÓ agregar a user $username a la db")
+                                    }
                             }
                         }
                         .addOnFailureListener {
                             Log.e("TAG", "FAILURELISTENER")
-                            validCode = false
+                            stopFunction = true
                         }
                 }
                 else -> null
             }
 
             task?.let {
-                Tasks.whenAllComplete(task).addOnCompleteListener {
-                    if (!validCode) {
-                        Log.d("TAG", "Se detectó un error y se detuvo la ejecución.")
+                Tasks.whenAllComplete(task)
+                    .addOnCompleteListener {
+                    if (stopFunction) {
+                        Log.e("ERROR", "EL CÓDIGO INGRESADO FALLÓOOO")
                         return@addOnCompleteListener
-                    }
-                    Log.d("TAG", "EL CODIGO SIGUIO DESPUES DEL RETURN")
-                }
-            }
+                    } else {
+                        auth.createUserWithEmailAndPassword(email.toString(), password.toString())
+                            .addOnCompleteListener(this) { task ->
+                                if (task.isSuccessful) {
+                                    user = auth.currentUser
+                                    userID = user?.uid
 
-
-
-            /*
-
-            val userData = hashMapOf(
-                "username" to username,
-                "email" to email,
-                "role" to roleString,
-                "timeCreation" to Timestamp.now()
-            )
-
-            if (roleInt == 1) {
-                val randomCode = generateRandomCodeRestaurant()
-                val restaurantName = binding.editTextNameOrCode.text.toString().trim()
-
-                db.collection("restaurants").document(randomCode).get()
-                    .addOnSuccessListener { document ->
-                        if (!document.exists()) {
-                            val restaurantData = hashMapOf(
-                                "name" to restaurantName,
-                                "adminID" to userID,
-                                "timeCreation" to Timestamp.now()
-                            )
-
-
-
-                        } else {
-
-                        }
-                    }
-
-                db.collection("users").document(userID.toString()).set(userData)
-                    .addOnSuccessListener {
-                        Log.d("TAG", "Admin $username")
-                    }
-                    .addOnFailureListener {
-                        Log.d("TAG", "Usuario $username NO asociado con restaurantes")
-                    }
-
-
-            } else {
-                val restaurantCode = binding.editTextNameOrCode.text.toString().trim()
-
-                db.collection("users").document(userID.toString()).set(userData)
-                    .addOnSuccessListener {
-                        Log.d("TAG", "Usuario $username creado y asociado con éxito a restaurante $restaurantCode")
-                    }
-                    .addOnFailureListener {
-                        Log.d("TAG", "Usuario $username NO asociado con restaurantes")
-                    }
-
-                db.collection("restaurants").document(restaurantCode).get()
-                    .addOnSuccessListener { document ->
-                        if (!document.exists()) {
-                            binding.root.alert("Código ingresado no existe.")
-                             return@addOnSuccessListener
-                        } else {
-                           db.collection("users").document(userID.toString())
-                               .set()
-                        }
-
-                    }
-            }
-
-            db.collection("users").document(userID.toString())
-                .update("restaurantID", restaurantCode)
-                .addOnSuccessListener {
-                    Log.d("TAG",
-                        "usuario asociado con restaurante" +
-                                "user: $username | restaurantecode: $restaurantCode")
-
-                    this.moveTo(TableMapActivity::class.java)
-                }
-                .addOnFailureListener {
-                    Log.d("ERROR",
-                        "falló asociar usuario y restaurant")
-                }
-
-            Log.e("TAG", "El programa siguió")
-
-
-
-
-             */
-
-
-
-            /*
-            auth.createUserWithEmailAndPassword(email.toString(), password.toString())
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-
-                        val user = auth.currentUser
-                        val userID = user?.uid
-
-                        val userData = hashMapOf(
-                            "username" to username,
-                            "email" to email,
-                            "role" to roleString,
-                            "restaurantID" to randomCode,
-                            "timeCreation" to Timestamp.now()
-                        )
-                        // AQUÍ FALTA QUE SE UNA A UN RESTAURANTE EXISTENTE (when 2, 3)
-                        when (roleInt) {
-                            1 -> {
-                                Log.d("ERROR", "ROL ES ADMINISTRADOOOOOOOOOR")
-                                val restaurantData = hashMapOf(
-                                    "name" to nameOrCode,
-                                    "adminID" to userID,
-                                    "timeCreation" to Timestamp.now()
-                                )
-
-                                db.collection("restaurants").document(randomCode).set(restaurantData)
-                                    .addOnSuccessListener {
-                                        Log.d("TAG",
-                                            "FUNCIONÓ, name: $nameOrCode | code: $randomCode ")
-                                    }
-                                    .addOnFailureListener {
-                                        Log.d("ERROR",
-                                            "falló agregar restaurant a db")
-                                    }
-                            }
-                            else -> {
-                                Log.d("ERROR", "CUALQUIER OTRO ROOOOOOOOL")
-                                randomCode = nameOrCode
-
-                                db.collection("restaurants").document(randomCode).get()
-                                    .addOnSuccessListener {
-                                        if (it.exists()) {
-                                            validRestaurant = true
-                                            Log.d("ERROR", "RESTAURANTE SI EXISTEEEEE")
-                                            db.collection("users").document(userID.toString())
-                                                .update("restaurantID", randomCode)
-                                                .addOnSuccessListener {
-                                                    Log.d("TAG",
-                                                        "usuario asociado con restaurante" +
-                                                                "user: $username | restaurantecode: $nameOrCode")
-                                                }
-                                                .addOnFailureListener {
-                                                    Log.d("ERROR",
-                                                        "falló asociar usuario y restaurant")
-                                                }
-                                        } else {
-                                            Log.d("ERROR", "NO EXISTE RESTAURANTEEEEEE")
-                                            validRestaurant = false
-                                            binding.linearLayout.visibility = View.VISIBLE
-                                            binding.linearLayoutNameCode.visibility = View.VISIBLE
-                                            binding.progressBar.visibility = View.GONE
-                                            binding.back.isClickable = true
-
-                                            binding.root.alert("No existe restaurante con ese código. Intente de nuevo")
-                                            return@addOnSuccessListener
+                                    if (roleInt == 1) {
+                                    db.collection("restaurants").document(restaurantCode)
+                                        .update("adminID", userID)
+                                        .addOnSuccessListener {
+                                            Log.d("TAG", "ACTUALIZACIÓN adminID SE REALIZÓ")
+                                        }
+                                        .addOnFailureListener {
+                                            Log.e("ERROR", "NO SE PUDO ACTUALIZAR adminID")
                                         }
                                     }
-                            }
-                        }
 
-                        if (!validRestaurant) {
-                            Log.d("ERROR", "VALID RESTAURANT FAAAAAAAAAAALSE")
-                            return@addOnCompleteListener
-                        }
-
-                        userID?.let {
-                            db.collection("users").document(it).set(userData)
-                                .addOnSuccessListener {
-                                    Log.d("ERROR", "USERID LET SUCCESS")
                                     this.moveTo(TableMapActivity::class.java)
-                                    Functions.clearData(this, "temp_data")
-                                    finish()
-                                }
-                                .addOnFailureListener {
-                                    Log.d("ERROR", "USERID LET FAILURE")
+                                } else {
                                     binding.root.alert(getString(R.string.failed_database))
-                                    binding.buttonSignUp.isClickable = true
                                 }
-                        }
-                        Log.d("ERROR", "ultimo validRestaurant = $validRestaurant")
-
-                    } else {
-                        binding.root.alert(getString(R.string.failed_sign_up))
-                        binding.buttonSignUp.isClickable = true
-
+                            }
                     }
                 }
-
-             */
+            }
         }
     }
 
@@ -332,18 +217,31 @@ class SelectRoleActivity : AppCompatActivity(), AdapterView.OnItemSelectedListen
         .joinToString("")
     // esto genera el código para el restaurante de 6 caracteres
 
+    private fun createRandomRestaurantID() {
+        val db = FirebaseFirestore.getInstance()
+        val randomCode = generateRandomCodeRestaurant()
 
-    private fun addUserToDB() {
+        db.collection("restaurants").document(randomCode).get()
+            .addOnSuccessListener { document ->
+                if (!document.exists()) {
+                    val restaurantData = hashMapOf(
+                        "name" to restaurantName,
+                        "timeCreation" to Timestamp.now()
+                    )
 
+                    db.collection("restaurants").document(randomCode).set(restaurantData)
+                        .addOnSuccessListener {
+                            Log.d("TAG", "Restaurante CREADO. $restaurantName código $restaurantCode")
+                        }
+                        .addOnFailureListener {
+                            Log.e("ERROR", "Creación Restaurante FALLIDA.")
+                        }
+                } else {
+                    createRandomRestaurantID()
+                }
+            }
+            .addOnFailureListener {
+                Log.e("ERROR", "Error en uniqueRandomCode()!!!!!!!!")
+            }
     }
-
-    private fun addRestaurantToDB() {
-
-    }
-
-    private fun userAuthFirebase() {
-
-    }
-
-
 }
